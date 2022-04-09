ed8b919f7d4ae1cb44f64a6ceeace2899faab640
xultan1 <xultanjj@gmail.com>
2022-04-09 22:02:02 +0100

    nc

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for mysite project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It expose the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
