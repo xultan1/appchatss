506b0bf1ded49b019f87ec55ca635e6523e3d1a9
xultan1 <xultanjj@gmail.com>
2022-04-10 21:31:59 +0100

    nc

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -3,14 +3,22 @@ import os
 
 import django
 
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
 
-
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 django.setup()
 
 application = ProtocolTypeRouter({
