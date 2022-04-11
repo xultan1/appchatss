8f33ee5f6837cd14bc53ddb5704457242be215cf
xultan1 <xultanjj@gmail.com>
2022-04-11 07:43:15 +0100

    nc

---
+++ chat/routing.py
@@ -0,0 +1,12 @@
+# chat/routing.py
+from django.urls import re_path
+
+from django.urls import path
+
+from chat import consumers
+
+websocket_urlpatterns = [
+    #re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<chats_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/all/new/(?P<id>\w+)/new/$', consumers.ChatConsumerAll.as_asgi()),
+]
