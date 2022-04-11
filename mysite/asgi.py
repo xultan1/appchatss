cddb6705665748a9c19bd97350e5ba2732f841f8
xultan1 <xultanjj@gmail.com>
2022-04-11 10:03:37 +0100

    nc

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,33 +1,41 @@
-# mysite/asgi.py
+## mysite/asgi.py
+#import os
+#
+#import django
+#
+##os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#
+#from django.conf import settings
+#
+#from channels.auth import AuthMiddlewareStack
+#from channels.routing import ProtocolTypeRouter, URLRouter
+#from django.core.asgi import get_asgi_application
+#from chat import routing
+#
+#
+#
+#
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#
+#
+#
+##os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+##settings.configure()
+#django.setup()
+#
+#application = ProtocolTypeRouter({
+#  "http": get_asgi_application(),
+#  "websocket": AuthMiddlewareStack(
+#        URLRouter(
+#            routing.websocket_urlpatterns
+#        )
+#    ),
+#})
+
 import os
-
 import django
+from channels.routing import get_default_application
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-from django.conf import settings
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from chat import routing
-
-
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-
-
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-#settings.configure()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
-
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_default_application()
