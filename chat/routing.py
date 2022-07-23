bdedd2e727efe5aba90775c599dbefd3d9452fb5
michaels1968 <xultanjj@gmail.com>
2022-07-23 11:00:27 -0700

    Update routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -9,4 +9,5 @@ websocket_urlpatterns = [
     #re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/chat/(?P<chats_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/all/new/(?P<id>\w+)/new/$', consumers.ChatConsumerAll.as_asgi()),
+    re_path(r'ws/public/(?P<id>\w+)/$', consumers.ChatConsumerPublic.as_asgi()),
 ]
