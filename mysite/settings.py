59e0694e5ae657da78eeda78aa1e75a7a684c87e
raymon580 <87141242+Raymon580@users.noreply.github.com>
2023-08-01 20:03:15 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -169,14 +169,14 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL','16.171.146.12:6379')],
+            "hosts": [os.environ.get('REDIS_URL','16.171.146.12:6381')],
         },
     },
 }
 
 
 #CELERY_BROKER_URL = os.environ['REDIS_URL']
-CELERY_BROKER_URL = os.environ.get('REDIS_URL','16.171.146.12:6379')
+CELERY_BROKER_URL = os.environ.get('REDIS_URL','16.171.146.12:6381')
 #CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['application/json']
