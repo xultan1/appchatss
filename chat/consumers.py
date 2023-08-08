6793f0e0d4ee14dfa24178bde516721c7aa4c9e1
michaels1968 <petermichaels1968@gmail.comm>
2023-08-08 18:19:17 +0100

    Update consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -174,38 +174,73 @@ class ChatConsumerAll(AsyncWebsocketConsumer):
 
 
 class ChatConsumerPublic(AsyncWebsocketConsumer):
-    def connect(self):
+    async def connect(self):
         self.all_name = self.scope['url_route']['kwargs']['id']
         #self.room_group_name = 'chat_%s' % self.chats_name
         self.room_group_name = 'public_%s' % self.all_name
         # Join room group
-
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
 
     async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
         )
+        
 
-
-    def receive(self, text_data):
+    # Receive message from WebSocket
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        message = text_data_json['message']
+        self.user = self.scope["user"]
 
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
-        )
+        try:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': {"person":message["person"],"personMessaging":message["personMessaging"],"who":message["who"]},
+                }
+            )
+        except:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': {"person":message["person"]},
+                }
+            )
+
+    
 
     # Receive message from room group
-    def chat_message(self, event):
-        message = event["message"]
+    async def chat_message(self, event):
+        message = event['message']
+        self.user = self.scope["user"]
 
+        try:
+            if message["who"] == message["personMessaging"]:
+                return
+        except:
+            print("")
+
+
+
+
+        if message["person"] != str(self.user):
+            return
+
+
+
+    
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({
+            'message': message["personMessaging"],
+        }))
